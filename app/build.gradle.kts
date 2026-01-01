76ec541a595e2716df11de3db88b8203e1996ae9
Alain Knaff <alain@knaff.lu>
2026-01-01 23:52:39 +0100

    Signing config compatible with github

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,22 @@ android {
 
     defaultConfig {
         applicationId = "lu.knaff.alain.share_to_folder"
-        minSdk = 21
+	// minSdk 24 to avoid v1 signing which causes noisy warnings
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("keystore.jks")
+            storePassword = System.getenv("SIGNING_STORE_PASSWORD")
+            keyAlias = System.getenv("SIGNING_KEY_ALIAS")
+            keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
+        }
     }
 
     buildTypes {
@@ -25,8 +35,10 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-        }
+	    signingConfig = signingConfigs.getByName("release")
+	}
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -38,6 +50,11 @@ android {
     }
     buildFeatures {
         viewBinding = true
+    }
+
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
     }
 }
 

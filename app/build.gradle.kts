95a33d1c0676e8fa2c4d4738e9973d60401cbf1c
Alain Knaff <alain@knaff.lu>
2026-08-11 20:58:48 +0200

    handle files shared by Gallery, which neither have an extension on filename, nor an extension with the supplied Mime type

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 	// minSdk 24 to avoid v1 signing which causes noisy warnings
         minSdk = 24
         targetSdk = 36
-        versionCode = 104
-        versionName = "0.1.4"
+        versionCode = 105
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

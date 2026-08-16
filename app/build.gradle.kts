324e983f8507dda9c74c4bf579c1d1684b03b87a
Alain Knaff <alain@knaff.lu>
2026-08-16 20:53:28 +0200

    - Support for subdir mode: clicking on a shortcut doesn't save the shared directly under it, but instead opens up a file picker to chose a directory where to save starting at the shortcut's location.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
 	// minSdk 24 to avoid v1 signing which causes noisy warnings
         minSdk = 24
         targetSdk = 37
-        versionCode = 105
-        versionName = "0.1.5"
+        versionCode = 106
+        versionName = "0.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

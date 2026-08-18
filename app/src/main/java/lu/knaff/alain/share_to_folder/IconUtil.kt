6961d4c92ed8c919b435a571308b8c6f0a5982d0
Alain Knaff <alain@knaff.lu>
2026-08-18 20:55:35 +0200

    Removed trailing space

--- app/src/main/java/lu/knaff/alain/share_to_folder/IconUtil.kt
+++ app/src/main/java/lu/knaff/alain/share_to_folder/IconUtil.kt
@@ -9,7 +9,7 @@ import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.Bitmap
 import android.content.pm.PackageManager
-import android.content.pm.ApplicationInfo 
+import android.content.pm.ApplicationInfo
 import android.content.Context
 
 class IconUtil {
